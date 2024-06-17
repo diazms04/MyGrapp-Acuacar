3c9d986eb417a7d47e71059651158a7d1c25a3b4
Martin Diaz <92173560+diazms04@users.noreply.github.com>
2024-06-17 09:20:40 -0500

    Responsive scss

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,20 @@
-import React from 'react'
-import './NavBar.scss'
-import imglo from '../../assets/Logo Acuacar 2019.png'
-import logoPatio from '../../assets/patio.png'
+import React from 'react';
+import './NavBar.scss';
+import imglo from '../../assets/Logo Acuacar 2019.png';
+import logoPatio from '../../assets/patio.png';
 
-function NavBar({leftChildren}) {
+function NavBar({ leftChildren }) {
   return (
-    <>
-        <div className='nav_container'>
-          <div className='logos_pro'>
-            {leftChildren}
-        </div>
-
-        <div className='title_nav'>
-            <h1 className='titles'>My Grapp</h1>
-        </div>
-          
-        <div className='logos_pro'>
-
-        </div>
-        </div>
-    </>
-  )
+    <div className="nav_container">
+      <div className="logos_pro">
+        {leftChildren}
+      </div>
+      <div className="title_nav">
+        <h1 className="titles">My Grapp</h1>
+      </div>
+      <div className="logos_pro"></div>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
